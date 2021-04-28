a93225acd306408c2f24de9e54768a487c51d6da
Rex Ma <rexma1102@gmail.com>
2021-04-28 00:35:59 -0400

    add manifold anlignment

--- snf2/embedding.py
+++ snf2/embedding.py
@@ -9,13 +9,20 @@
 #  Copyright (c) 2008 Tilburg University. All rights reserved.
 
 import numpy as np
+from snf2.Model import model
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+import os
 
 
 def tsne_p(P=np.array([]), no_dims=50, initial_dims=50, perplexity=30.0):
     """
-        Runs t-SNE on the dataset in the NxN matrix P to extract embedding vectors
-        to no_dims dimensions. The syntaxis of the function is
-        `Y = tsne.tsne_p(P, no_dims, perplexity), where P is an NxN NumPy array.
+    Runs t-SNE on the dataset in the NxN matrix P to extract embedding vectors
+    to no_dims dimensions. The syntaxis of the function is
+    `Y = tsne.tsne_p(P, no_dims, perplexity), where P is an NxN NumPy array.
     """
 
     # Check inputs
@@ -41,10 +48,10 @@ def tsne_p(P=np.array([]), no_dims=50, initial_dims=50, perplexity=30.0):
     gains = np.ones((n, no_dims))
 
     # Make sure P-values are set properly
-    np.fill_diagonal(P, 0)     # set diagonal to zero
-    P = P + np.transpose(P)        # symmetrize P-values
-    P = P / np.sum(P)              # make sure P-values sum to one
-    P = P * 4.					   # early exaggeration
+    np.fill_diagonal(P, 0)  # set diagonal to zero
+    P = P + np.transpose(P)  # symmetrize P-values
+    P = P / np.sum(P)  # make sure P-values sum to one
+    P = P * 4.0  # early exaggeration
     P = np.maximum(P, 1e-12)
 
     # Run iterations
@@ -52,24 +59,27 @@ def tsne_p(P=np.array([]), no_dims=50, initial_dims=50, perplexity=30.0):
 
         # Compute pairwise affinities
         sum_Y = np.sum(np.square(Y), 1)
-        num = -2. * np.dot(Y, Y.T)
-        num = 1. / (1. + np.add(np.add(num, sum_Y).T, sum_Y))
-        num[range(n), range(n)] = 0.
+        num = -2.0 * np.dot(Y, Y.T)
+        num = 1.0 / (1.0 + np.add(np.add(num, sum_Y).T, sum_Y))
+        num[range(n), range(n)] = 0.0
         Q = num / np.sum(num)
         Q = np.maximum(Q, 1e-12)
 
         # Compute gradient
         PQ = P - Q
         for i in range(n):
-            dY[i, :] = np.sum(np.tile(PQ[:, i] * num[:, i], (no_dims, 1)).T * (Y[i, :] - Y), 0)
+            dY[i, :] = np.sum(
+                np.tile(PQ[:, i] * num[:, i], (no_dims, 1)).T * (Y[i, :] - Y), 0
+            )
 
         # Perform the update
         if iter < 20:
             momentum = initial_momentum
         else:
             momentum = final_momentum
-        gains = (gains + 0.2) * ((dY > 0.) != (iY > 0.)) + \
-                (gains * 0.8) * ((dY > 0.) == (iY > 0.))
+        gains = (gains + 0.2) * ((dY > 0.0) != (iY > 0.0)) + (gains * 0.8) * (
+            (dY > 0.0) == (iY > 0.0)
+        )
         gains[gains < min_gain] = min_gain
         iY = momentum * iY - eta * (gains * dY)
         Y = Y + iY
@@ -82,9 +92,116 @@ def tsne_p(P=np.array([]), no_dims=50, initial_dims=50, perplexity=30.0):
 
         # Stop lying about P-values
         if iter == 100:
-            P = P / 4.
+            P = P / 4.0
 
     # Return solution
     return Y
 
 
+def init_model(net, device, restore):
+    if restore is not None and os.path.exits(restore):
+        net.load_state_dict(torch.load(restore))
+        net.restored = True
+        print("Restore model from: {}".format(os.path.abspath(restore)))
+
+    else:
+        pass
+
+    if torch.cuda.is_available():
+        cudnn.benchmark = True
+        net.to(device)
+
+    return net
+
+
+def neg_square_dists(X):
+    sum_X = torch.sum(X * X, 1)
+    tmp = torch.add(-2 * X.mm(torch.transpose(X, 1, 0)), sum_X)
+    D = torch.add(torch.transpose(tmp, 1, 0), sum_X)
+    return -D
+
+
+def Q_tsne(Y):
+    distances = neg_square_dists(Y)
+    inv_distances = torch.pow(1.0 - distances, -1)
+    inv_distances = inv_distances - torch.diag(inv_distances.diag(0))
+    inv_distances = inv_distances + 1e-15
+    return inv_distances / torch.sum(inv_distances)
+
+
+def project_tsne(dataset, num_com, no_dims=50):
+
+    print("---------------------------------")
+    print("Begin finding the embedded space")
+
+    dataset_num = len(dataset)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    lr = 0.001
+    epoch_DNN = 500
+    log_DNN = 10
+    beta = 1
+
+    # get data dims
+    col = []
+    print("Shape of Raw data")
+    for i in range(dataset_num):
+        col.append(np.shape(dataset[i])[1])
+        print("Dataset {}:".format(i), np.shape(dataset[i]))
+        dataset[i] = torch.from_numpy(dataset[i]).float().to(device)
+
+    net = model(col, no_dims)
+    Project_DNN = init_model(net, device, restore=None)
+
+    optimizer = optim.RMSprop(Project_DNN.parameters(), lr=lr)
+    c_mse = nn.MSELoss()
+    Project_DNN.train()
+
+    for epoch in range(epoch_DNN):
+        KL_loss = []
+        for i in range(dataset_num):
+            P_tmp = dataset[i]
+            low_dim_data = Project_DNN(dataset[i], i)
+            Q_joint = Q_tsne(low_dim_data)
+
+            ## loss of structure preserving
+            KL_loss.append(torch.sum(P_tmp * torch.log(P_tmp / Q_joint)))
+
+        ## loss of structure matching
+        feature_loss = np.array(0)
+        feature_loss = torch.from_numpy(feature_loss).to(device).float()
+        for i in range(dataset_num - 1):
+            low_dim_set1 = Project_DNN(
+                dataset[i][
+                    0:num_com,
+                ],
+                i,
+            )
+            low_dim_set2 = Project_DNN(
+                dataset[dataset_num - 1][
+                    0:num_com,
+                ],
+                len(dataset) - 1,
+            )
+            feature_loss += c_mse(low_dim_set1, low_dim_set2)
+
+        loss = beta * feature_loss
+        for i in range(dataset_num):
+            loss += KL_loss[i]
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch + 1) % log_DNN == 0:
+        print(
+            "epoch:[{:d}/{}]: loss:{:4f}, align_loss:{:4f}".format(
+                epoch + 1, epoch_DNN, loss.data.item(), feature_loss.data.item()
+            )
+        )
+
+    integrated_data = []
+    for i in range(dataset_num):
+        integrated_data.append(Project_DNN(dataset[i], i))
+        integrated_data[i] = integrated_data[i].detach().cpu().numpy()
+    print("Done")
+    return integrated_data
--- tests/test_butterfly.py
+++ tests/test_butterfly.py
@@ -12,9 +12,10 @@ import matplotlib.pyplot as plt
 
 import sys
 import os
+
 d = os.path.dirname(os.getcwd())
 sys.path.insert(0, d)
-from snf2.embedding import tsne_p
+from snf2.embedding import tsne_p, project_tsne
 from snf2.main import dist2, snf2, kernel_matching
 from snf2.util import data_indexing
 
@@ -25,21 +26,41 @@ w2_ = os.path.join(testdata_dir, "w2.csv")
 w1 = pd.read_csv(w1_, index_col=0)
 w2 = pd.read_csv(w2_, index_col=0)
 
-label = ['label']
+label = ["label"]
 w1_label = w1[label]
 w2_label = w2[label]
-wcom_label = w1_label.filter(regex='^common_', axis=0)
+wcom_label = w1_label.filter(regex="^common_", axis=0)
 w1.drop(label, axis=1, inplace=True)
 w2.drop(label, axis=1, inplace=True)
 wall_label = pd.concat([w1_label, w2_label], axis=0)
-wall_label = wall_label[~wall_label.index.duplicated(keep='first')]
+wall_label = wall_label[~wall_label.index.duplicated(keep="first")]
+
+
+# print(w1.shape)
+# print(w2.shape)
+# np.savetxt(os.path.join(testdata_dir, 'w1.txt'), w1.values, delimiter=',')
+# np.savetxt(os.path.join(testdata_dir, 'w2.txt'), w2.values, delimiter=',')
+# np.savetxt(os.path.join(testdata_dir, 'w1_type.txt'), w1_label.values, delimiter=',')
+# np.savetxt(os.path.join(testdata_dir, 'w2_type.txt'), w2_label.values, delimiter=',')
+
+
+# np.savetxt(os.path.join(testdata_dir, 'wall_label.txt'), wall_label.values, delimiter=',')
+
+# S_final = final = np.loadtxt("/Users/mashihao/Desktop/SNF2/data/final.txt", delimiter=',')
+# Dist_final = dist2(S_final, S_final)
+# Wall_final = snf.compute.affinity_matrix(Dist_final, K=20, mu=0.5)
+
+# labels_final = spectral_clustering(Wall_final, n_clusters=10)
+# score = v_measure_score(wall_label['label'].tolist() , labels_final)
+# print("SNF2 + kernel matching for clustering union 1332 samples NMI score:", score)
+# assert 0
 
 
 """
     Step1 : Apply the original SNF on the common samples, and the score will be a reference point
 """
-w1_com = w1.filter(regex='^common_', axis=0)
-w2_com = w2.filter(regex='^common_', axis=0)
+w1_com = w1.filter(regex="^common_", axis=0)
+w2_com = w2.filter(regex="^common_", axis=0)
 
 # need to make sure the order of common samples are the same for all views before fusing
 dist1_com = dist2(w1_com.values, w1_com.values)
@@ -49,7 +70,7 @@ S2_com = snf.compute.affinity_matrix(dist2_com, K=20, mu=0.5)
 
 fused_network = snf.snf([S1_com, S2_com])
 labels_com = spectral_clustering(fused_network, n_clusters=10)
-score_com = v_measure_score(wcom_label['label'].tolist(), labels_com)
+score_com = v_measure_score(wcom_label["label"].tolist(), labels_com)
 print("Original SNF for clustering intersecting 832 samples NMI score: ", score_com)
 
 
@@ -64,11 +85,11 @@ S1 = snf.compute.affinity_matrix(Dist1, K=20, mu=0.5)
 S2 = snf.compute.affinity_matrix(Dist2, K=20, mu=0.5)
 
 labels_s1 = spectral_clustering(S1, n_clusters=10)
-score1 = v_measure_score(w1_label['label'].tolist(), labels_s1)
+score1 = v_measure_score(w1_label["label"].tolist(), labels_s1)
 print("Before diffusion for full 1032 p1 NMI score: ", score1)
 
 labels_s2 = spectral_clustering(S2, n_clusters=10)
-score2 = v_measure_score(w2_label['label'].tolist(), labels_s2)
+score2 = v_measure_score(w2_label["label"].tolist(), labels_s2)
 print("Before diffusion for full 1132 p2 NMI score:", score2)
 
 # Do SNF2 diffusion
@@ -76,29 +97,32 @@ dicts_common, dicts_unique, original_order = data_indexing([w1, w2])
 S1_df = pd.DataFrame(data=S1, index=original_order[0], columns=original_order[0])
 S2_df = pd.DataFrame(data=S2, index=original_order[1], columns=original_order[1])
 
-fused_networks = snf2([S1_df, S2_df], 
-                      dicts_common=dicts_common,
-                      dicts_unique=dicts_unique,
-                      original_order=original_order)
+fused_networks = snf2(
+    [S1_df, S2_df],
+    dicts_common=dicts_common,
+    dicts_unique=dicts_unique,
+    original_order=original_order,
+)
 
 S1_fused = fused_networks[0]
 S2_fused = fused_networks[1]
 
 labels_s1 = spectral_clustering(S1_fused.values, n_clusters=10)
-score1 = v_measure_score(w1_label['label'].tolist(), labels_s1)
+score1 = v_measure_score(w1_label["label"].tolist(), labels_s1)
 print("After diffusion for full 1032 p1 NMI score: ", score1)
 
 labels_s2 = spectral_clustering(S2_fused.values, n_clusters=10)
-score2 = v_measure_score(w2_label['label'].tolist(), labels_s2)
+score2 = v_measure_score(w2_label["label"].tolist(), labels_s2)
 print("After diffusion for full 1132 p2 NMI score:", score2)
 
 """
     Step3 : Use the t-SNE to extract embedding vectors from similarity networks
 """
-#w1_tsne = tsne_p(S1_fused.values, no_dims=20)
-#w2_tsne = tsne_p(S2_fused.values, no_dims=20)
-#np.savetxt("/Users/mashihao/Desktop/SNF2/data/w1_tsne.csv", w1_tsne, delimiter=",")
-#np.savetxt("/Users/mashihao/Desktop/SNF2/data/w2_tsne.csv", w2_tsne, delimiter=",")
+# w1_tsne = tsne_p(S1_fused.values, no_dims=20)
+# w2_tsne = tsne_p(S2_fused.values, no_dims=20)
+# np.savetxt("/Users/mashihao/Desktop/SNF2/data/w1_tsne.csv", w1_tsne, delimiter=",")
+# np.savetxt("/Users/mashihao/Desktop/SNF2/data/w2_tsne.csv", w2_tsne, delimiter=",")
+w1_tsne = project_tsne([S1_fused.values, S2_fused.values], num_com=832, no_dims=20)
 
 # load t-sne
 tsne_w1 = os.path.join(testdata_dir, "w1_tsne.csv")
@@ -114,12 +138,13 @@ w2_tsne = np.loadtxt(tsne_w2, delimiter=",")
 embed_w1 = pd.DataFrame(data=w1_tsne, index=original_order[0])
 embed_w2 = pd.DataFrame(data=w2_tsne, index=original_order[1])
 
+
 S_final = kernel_matching(
     [embed_w1, embed_w2],
     dicts_common=dicts_common,
     dicts_unique=dicts_unique,
-    alpha=0.1, 
-    matching_iter=50
+    alpha=0.1,
+    matching_iter=50,
 )
 S_final = S_final.reindex((wall_label.index.tolist()), axis=0)
 
@@ -127,17 +152,43 @@ Dist_final = dist2(S_final.values, S_final.values)
 Wall_final = snf.compute.affinity_matrix(Dist_final, K=20, mu=0.5)
 
 labels_final = spectral_clustering(Wall_final, n_clusters=10)
-score = v_measure_score(wall_label['label'].tolist() , labels_final)
+score = v_measure_score(wall_label["label"].tolist(), labels_final)
 print("SNF2 + kernel matching for clustering union 1332 samples NMI score:", score)
 
+
+# # lets do some ploting
+# X_embedded = TSNE(n_components=2).fit_transform(S_final.values)
+# plt.scatter(
+#     X_embedded[:, 0],
+#     X_embedded[:, 1],
+#     c=wall_label["label"].tolist(),
+#     s=1.5,
+#     cmap="Spectral",
+# )
+# plt.title("t-SNE visualization of union 1332 butterfly")
+# save_path = os.path.join(testdata_dir, "t-SNE_butterfly.png")
+# plt.savefig(save_path)
+# print("Save visualization at {}".format(save_path))
+
+
 """
+# plot TSNE before matching
+all_embed = pd.concat([embed_w1, embed_w2], axis=0)
+all_label = pd.concat([w1_label, w2_label], axis=0)
+
+print(all_embed.shape)
+print(all_label.shape)
 # lets do some ploting
-X_embedded = TSNE(n_components=2, metric='precomputed').fit_transform(2 - final_result.values)
-#X_embedded = tsne_p(Wall_final, no_dims=2)
-plt.scatter(X_embedded[:, 0], X_embedded[:, 1], c=alllabels, s=1.5, cmap='Spectral')
-plt.title('t-SNE visualization of union 1332 butterfly after batch correction')
-save_path = op.join(testdata_dir, 't-SNE_1332_really.png')
+X_embedded = TSNE(n_components=2).fit_transform(all_embed.values)
+plt.scatter(
+    X_embedded[:, 0],
+    X_embedded[:, 1],
+    c=all_label["label"].tolist(),
+    s=1.5,
+    cmap="Spectral",
+)
+plt.title("t-SNE visualization of both view1 and view2 before matching")
+save_path = os.path.join(testdata_dir, "t-SNE_before_matching.png")
 plt.savefig(save_path)
 print("Save visualization at {}".format(save_path))
 """
-
